f14bf12027aae0293a371b9d74cf96731c1a9e3e
davesade <davesade.42@gmail.com>
2018-03-03 22:50:03 +0100

    Working autorun of node app.

--- 1_EC2/sample_cli.sh
+++ 1_EC2/sample_cli.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-aws ec2 run-instances --image-id ami-5652ce39 --instance-type t2.micro --key-name zu697-0451.pem --tag-specifications 'ResourceType=instance,Tags=[{Key=Application,Value=Bazinga},{Key=CostCenter,Value=6522},{Key=Name,Value=TestInstance}]' --user-data file://advanced.txt
+aws ec2 run-instances --image-id ami-0f60ae938604881a6 --instance-type t2.micro --key-name zu697-0451.pem --tag-specifications 'ResourceType=instance,Tags=[{Key=Product,Value=Ledger},{Key=CostCenter,Value=6522},{Key=Name,Value=TrainingInstance}]' --user-data file://advanced.txt --iam-instance-profile Name="trainingRoleEC2"
